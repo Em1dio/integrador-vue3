635bb3611cf4172d3dcb2524013139bd54a046dc
Caio Emidio <caio.emidio@gmail.com>
2024-05-31 19:47:27 +0100

    fist commit

---
+++ pages/livebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-screen bg-[#f1652a] h-12">
+    <div class="px-10 h-full flex items-center gap-4 justify-between">
+      <div class="flex items-center gap-4">
+        <div
+          class="h-full p-2 bg-[#181818] flex items-center justify-center rounded"
+        >
+          <icon name="logos:nuxt-icon" class="size-10" />
+        </div>
+        <p class="text-2xl font-semibold text-white">Em1dio</p>
+      </div>
+      <div class="flex gap-4">
+        <p class="text-2xl font-semibold text-white">!discord</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
